a14a5f9ff08f651e7b45bc7f064a7899f8543d83
Napatsakorn Pianchana <69056443+firstnapat@users.noreply.github.com>
2022-07-15 04:00:58 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/base64"
+  "fmt"
+  "strings"
+)
+
+func Reverse(s string) string {
+    n := len(s)
+    runes := make([]rune, n)
+    for _, rune := range s {
+        n--
+        runes[n] = rune
+    }
+    return string(runes[n:])
+}
+func main() {
+  sd, err := base64.StdEncoding.DecodeString("aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Decoded text: %s\n", Reverse(string(sd)))   
+    decodeText := strings.ReplaceAll(Reverse(string(sd)), ":", " ")
+    fmt.Println(decodeText)
+}
+  
